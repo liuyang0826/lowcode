72f719784c90d4445a32dbb28ac0eaeb722a2725
liuyang.0826 <1161906592@qq.com>
2025-02-07 09:12:18 +0800

    update

--- canvas/assets/remoteEntry.js
+++ canvas/assets/remoteEntry.js
@@ -1,1 +1,1 @@
-import{g as r,i as a}from"./_virtual___federation_remote-8f487aea.js";import"./federation-shared-1482b2fe.js";export{r as get,a as init};
+import{g as r,i as a}from"./_virtual___federation_remote-8445edb7.js";import"./federation-shared-1482b2fe.js";export{r as get,a as init};
